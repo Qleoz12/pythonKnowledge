668a1ccb75b191dbbe61584f97a4e20a943bdcab
qleoz12 <kintaroloop@gmail.com>
2022-12-26 10:07:23 -0500

    agregado de funciones

---
+++ lorenzo.py
@@ -0,0 +1,19 @@
+
+lorenzo="yellow"
+
+def funcion_interna(lorenzo):
+    # lorenzo="pig"
+    return lorenzo
+
+
+
+if __name__ == '__main__':
+    # arreglo=range(1,10)
+    # for i in arreglo:
+    #     print(i)
+    # print(arreglo.__str__())
+
+
+    print(lorenzo)
+
+    print(funcion_interna("rojo"))
