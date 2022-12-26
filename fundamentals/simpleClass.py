dc5590232031dbf1ca421e560cddc6e7bd23b485
qleoz12 <kintaroloop@gmail.com>
2022-12-26 10:11:41 -0500

    pruebas

--- fundamentals/simpleClass.py
+++ fundamentals/simpleClass.py
@@ -1,18 +1,64 @@
+"""
+clase =
+objeto =
+
+sobreesritura =
+"""
+
+
+
+class perrito:
+    pass
+
+class gato:
+    pass
+
+class tortuga:
+    pass
+
 class Person:
 
     def __init__(self,name,age):
         self.name= name
         self.age = age
 
+    # sobreescritura de un metodo nativo
+    def __str__(self):
+        return '<%s => %s>' % (self.__class__.__name__,self.name)
+
+    #metodo de clase
+    def saludar(self):
+        print(" hola buen día  me llamo " + self.name +" y tengo "+ str(self.age) +" años")
 
 
+def mysaludo(name : str ,edad: int,forma: str):
+    print(forma.format(name,edad))
 
 if __name__ == '__main__':
 
-    Lorenzo = Person("lorenzo",15)
+    lorenzo = Person("lorenzo",15)
+
+    pablo = Person("juan", 30)
+
+    saludo = "¿ Qué me cuentas?  me recuerdas  soy {}  de {}  "
+    #
+    # print(lorenzo.name)
+    # print(lorenzo.age)
+    #
+    # print(lorenzo.__str__())
+    #
+    # print(pablo.__str__())
+    #
+    # lorenzo.saludar()
+    #
+    # pablo.saludar()
+    #
+    list_personas=[lorenzo,pablo]
+
+    # iterador
+    for i in list_personas:
+        i.saludar()
+        mysaludo(i.name,i.age,saludo)
 
 
-    print(Lorenzo.name)
-    print(Lorenzo.age)
 
-    print(Lorenzo.__str__())
